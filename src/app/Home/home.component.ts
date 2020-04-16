21bd81f67dbdd9d638f1982182cfc47359244af1
StephenBoland <36884238+StephenBoland@users.noreply.github.com>
2020-04-16 17:53:24 +0100

    Improved Styles

---
+++ src/app/Home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component } from "@angular/core";
+import { Subscription } from "rxjs";
+import { UserAuthService } from "../userauth/userauth.service";
+@Component({
+  selector: 'app-home',
+  templateUrl: '/home.component.html',
+  styleUrls: ['/home.component.css']
+
+})
+export  class HomeComponent {
+  userAuthenticated = false; //default authentication is false
+  private AuthListenerSubscription : Subscription;
+
+  constructor(private UserAuthService: UserAuthService ) {}
+
+  ngOnInit () {
+    this.userAuthenticated = this.UserAuthService.UserAuthenticated(); //refreshing authentication
+    this.AuthListenerSubscription = this.UserAuthService.getAuthenticationStatus().subscribe(Authenticated => { //changing the user to being authenticated
+    this.userAuthenticated = Authenticated;
+    });
+  }
+//logging out
+  userLogout() {
+    this.UserAuthService.logout();
+
+  }
+
+  ngOnDestroy () {
+    this.AuthListenerSubscription.unsubscribe();
+
+  }
+}
